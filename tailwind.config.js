4a1310331c649ddba1d14d8d46d448bac54a87e4
Andry <andrydominguez90@gmail.com>
2021-09-01 17:28:30 -0400

    all

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontSize: {
+      tiny: '1em',
+      base: '2em',
+      big: '3em'
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
